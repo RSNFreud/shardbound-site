82ca160cbea146413044c761e2cb13ea29f6e2b6
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2023-11-29 22:20:11 +0200

    update constants.ts and home.tsx

--- src/_pages/homepage/constants.ts
+++ src/_pages/homepage/constants.ts
@@ -2,6 +2,14 @@ import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import Jay from './images/jay.webp'
 import Freud from './images/freud.webp'
 import Matty from './images/matty.webp'
+import Breadge from './images/breadge.webp'
+import Fox from './images/fox.webp'
+import Bonne from './images/bonne.webp'
+import Levi from './images/levi.webp'
+import Lux from './images/Lux.webp'
+import Pyder from './images/pyder.webp'
+import Bonker from './images/bonker.webp'
+import Wright from './images/wright.webp'
 
 export const roles = [
     "Development Managers",
@@ -12,7 +20,7 @@ export const roles = [
 ] as const;
 
 export const TEAM_MEMBERS: {
-    group: typeof roles[number];
+    group: typeof roles[number][];
     image?: string | StaticImport
     role: string,
     name: string,
@@ -20,10 +28,11 @@ export const TEAM_MEMBERS: {
     discord?: string
     twitter?: string
     twitch?: string
+    reddit?: string
     youtube?: string
 }[] = [
         {
-            group: "Development Managers",
+            group: ["Development Managers"],
             role: "Lead",
             image: Jay,
             name: "Jay",
@@ -31,7 +40,7 @@ export const TEAM_MEMBERS: {
             discord: "380999267524935680"
         },
         {
-            group: "Development Managers",
+            group: ["Development Managers"],
             role: "Development & Design Manager",
             name: "Freud",
             image: Freud,
@@ -41,11 +50,86 @@ export const TEAM_MEMBERS: {
             discord: "628703380851916827"
         },
         {
-            group: "Development Managers",
+            group: ["Development Managers"],
             role: "Build & Art Manager",
             image: Matty,
             name: "Matty",
             location: "United Kingdom",
             discord: "167229893527076864"
+        },
+        {
+            group: ["Artists"],
+            role: "Artist",
+            image: Breadge,
+            name: "Bread",
+            location: "Taiwan",
+            discord: "587623686714949655"
+        },
+        {
+            group: ["Artists", "Builders"],
+            role: "Artist & Builder",
+            image: Fox,
+            name: "Fox",
+            location: "Italy",
+            twitch: "TheFoxGamer20210",
+            youtube: "TheFoxGamer20210",
+            twitter: "TheFoxGamer20210",
+            discord: "760842243471966248"
+        },
+        {
+            group: ["Mod Developers"],
+            role: "Mod Developer",
+            image: Bonne,
+            name: "BONNe",
+            location: "Latvia",
+            discord: "488608806356516866"
+        },
+        {
+            group: ["Mod Developers"],
+            role: "Mod Developer",
+            name: "Leo",
+            location: "Italy",
+            discord: "536994802529599509",
+            reddit: "leyo05"
+        },
+        {
+            group: ["Builders"],
+            role: "Builder",
+            name: "Levi",
+            image: Levi,
+            location: "Netherlands",
+            discord: "787707627039621150",
+        },
+        {
+            group: ["Designers"],
+            role: "Designer",
+            name: "Lux Caeruleus",
+            image: Lux,
+            location: "Philippines",
+            discord: "463577575533314049",
+        },
+        {
+            group: ["Builders", "Artists"],
+            role: "Artist & Builder",
+            name: "Pydermandy",
+            image: Pyder,
+            location: "United States",
+            discord: "230121806734819329",
+        },
+        {
+            group: ["Mod Developers"],
+            role: "Mod Developer",
+            name: "bonker",
+            image: Bonker,
+            location: "United States",
+            discord: "772107911265910817",
+        },
+        {
+            group: ["Builders"],
+            role: "Builder",
+            name: "Master Wright",
+            image: Wright,
+            location: "United States",
+            discord: "346709211666972682",
         }
     ]
--- src/_pages/homepage/home.tsx
+++ src/_pages/homepage/home.tsx
@@ -5,7 +5,7 @@ import s from "./home.module.scss";
 import classNames from "classnames";
 import { TEAM_MEMBERS, roles } from "./constants";
 import Link from "next/link";
-import { FaDiscord, FaTwitch, FaYoutube } from "react-icons/fa";
+import { FaDiscord, FaReddit, FaTwitch, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import Image from "next/image";
 import { StaticImage } from "@/_components/staticImage";
@@ -27,54 +27,63 @@ export const Home = () => {
         ))}
       </div>
       <div className={s.grid}>
-        {TEAM_MEMBERS.filter((member) => member.group === activeRole).map(
-          (member) => (
-            <div className={s.teamMember} key={member.name}>
-              <div className={s.profilePic}>
-                {typeof member.image !== "undefined" && (
-                  <StaticImage src={member.image} alt="" fill />
-                )}
-              </div>
-              <div className={s.name}>{member.name}</div>
-              <div className={s.location}>{member.location}</div>
-              <div className={s.memberRole}>{member.role}</div>
-              <div className={s.icons}>
-                {Boolean(member.discord) && (
-                  <Link
-                    href={`https://discord.com/users/${member.discord}`}
-                    target="_blank"
-                  >
-                    <FaDiscord />
-                  </Link>
-                )}
-                {Boolean(member.twitter) && (
-                  <Link
-                    href={`https://twitter.com/${member.twitter}`}
-                    target="_blank"
-                  >
-                    <FaXTwitter />
-                  </Link>
-                )}
-                {Boolean(member.twitch) && (
-                  <Link
-                    href={`https://twitch.com/${member.twitch}`}
-                    target="_blank"
-                  >
-                    <FaTwitch />
-                  </Link>
-                )}
-                {Boolean(member.youtube) && (
-                  <Link
-                    href={`https://youtube.com/${member.youtube}`}
-                    target="_blank"
-                  >
-                    <FaYoutube />
-                  </Link>
-                )}
-              </div>
+        {TEAM_MEMBERS.filter(
+          (member) =>
+            member.group.filter((group) => group === activeRole).length
+        ).map((member) => (
+          <div className={s.teamMember} key={member.name}>
+            <div className={s.profilePic}>
+              {typeof member.image !== "undefined" && (
+                <StaticImage src={member.image} alt="" fill />
+              )}
             </div>
-          )
-        )}
+            <div className={s.name}>{member.name}</div>
+            <div className={s.location}>{member.location}</div>
+            <div className={s.memberRole}>{member.role}</div>
+            <div className={s.icons}>
+              {Boolean(member.discord) && (
+                <Link
+                  href={`https://discord.com/users/${member.discord}`}
+                  target="_blank"
+                >
+                  <FaDiscord />
+                </Link>
+              )}
+              {Boolean(member.twitter) && (
+                <Link
+                  href={`https://twitter.com/${member.twitter}`}
+                  target="_blank"
+                >
+                  <FaXTwitter />
+                </Link>
+              )}
+              {Boolean(member.twitch) && (
+                <Link
+                  href={`https://twitch.com/${member.twitch}`}
+                  target="_blank"
+                >
+                  <FaTwitch />
+                </Link>
+              )}
+              {Boolean(member.youtube) && (
+                <Link
+                  href={`https://youtube.com/${member.youtube}`}
+                  target="_blank"
+                >
+                  <FaYoutube />
+                </Link>
+              )}
+              {Boolean(member.reddit) && (
+                <Link
+                  href={`https://www.reddit.com/user/${member.reddit}`}
+                  target="_blank"
+                >
+                  <FaReddit />
+                </Link>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
