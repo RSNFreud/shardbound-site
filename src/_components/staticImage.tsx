fbc221847df00787b6ca7f355011c5e696be4d56
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2023-11-29 04:32:57 +0200

    update staticImage.tsx

--- src/_components/staticImage.tsx
+++ src/_components/staticImage.tsx
@@ -1,5 +1,10 @@
 import Image, { ImageProps } from "next/image";
 
 export const StaticImage = ({ src, ...rest }: ImageProps) => {
-  return <Image src={"/shardbound-site/" + src} {...rest} />;
+  return (
+    <Image
+      src={typeof src === "string" ? "/shardbound-site/" : "" + src}
+      {...rest}
+    />
+  );
 };
